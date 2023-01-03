6b9df6a3e081edddcd95dddf7896cdb861033b13
Christian Square <christiansquare20@gmail.com>
2023-01-03 05:29:00 -0600

    imported and installed axios on react app and added and installed Cors to settings on Songs Api.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const[AddMusic, setAddMusic]= useState([]);
+useEffect(()=>{
+  fetchAddMusic();
+},[]);
+
+
+async function fetchAddMusic(){
+  let response = await axios.get("http://127.0.0.1:8000/api/songs/");
+  console.log(response.data);
+  debugger;
+  setAddMusic(response.data);
+}
+    
+    
+    return (
+    <div>
+      {AddMusic.map((el)=>{
+        return<h1>{el.title}</h1>
+      })}
     </div>
   );
 }
